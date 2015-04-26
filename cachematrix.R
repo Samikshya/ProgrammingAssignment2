747caa34bd635a829e20c2ae85eccac70be6bec1
samikshya <aximas16@gmail.com>
2015-04-26 17:54:19 -0500

    Programming-Assignment2 - cacheMatrix
    It's Week 3 Programming Assignment for class R-Programming. Two
    functions makeCacheMatrix() and cacheSolve() were written and also added
    an example in readme file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+#function to create and store matrix.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+        
+        # holds the cached value or NULL if nothing is cached
+        cache <- NULL
+        
+        # store a new matrix
+        setMatrix <- function(newValue) {
+                x <<- newValue
+                # since new value is assigned, clear the cache
+                cache <<- NULL
+        }
+        
+        # returns the stored matrix
+        getMatrix <- function() {
+                x
+        }
+        
+        # cache the given argument 
+        cacheInverse <- function(solve) {
+                cache <<- solve
+        }
+        
+        # get the cached value
+        getInverse <- function() {
+                cache
+        }
+        
+        # return each element
+        list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+
+
+# function to calculate inverse  matrix created with makeCacheMatrix
+
+cacheSolve <- function(y, ...) {
+        
+        # get the cached value
+        inverse <- y$getInverse()
+        
+        # if cached value exists return the cached
+        if(!is.null(inverse)) {
+                message("Returned from cached data")
+                return(inverse)
+        }
+        
+        # if there is no cached value, caclulate the inverse and cache
+        data <- y$getMatrix()
+        
+        inverse <- solve(data)
+        
+        y$cacheInverse(inverse)
+        
+        # return inverse
+        inverse
 }
